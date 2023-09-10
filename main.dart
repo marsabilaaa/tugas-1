a952944bd0f71961204f5e33fda89cdecc551429
Marsa Salsabila <102282024+marsabilaaa@users.noreply.github.com>
2023-09-10 07:16:08 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -2,14 +2,16 @@ class Mahasiswa {
   String nim;
   String nama;
   int tahunLahir;
-
+  
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama dengan nim $nim dan umur ");
+  int umur = DateTime.now().year - tahunLahir;
+    print("Perkenalkan, nama saya $nama dengan nim $nim dan usia saya $umur ");
   }
 }
 
 void main() {
   var mhs = Mahasiswa("H1D021098", "Marsa Salsabila", 2003);
+mhs.perkenalan();
 }
